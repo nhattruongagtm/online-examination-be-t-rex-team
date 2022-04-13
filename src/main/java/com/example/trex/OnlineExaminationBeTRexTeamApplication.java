82910a26e9c22118a31239444ea255b07b89aa8b
nhattruongagtm <18130261@st.hcmuaf.edu.vn>
2022-04-13 07:51:43 +0700

    create project

---
+++ src/main/java/com/example/trex/OnlineExaminationBeTRexTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.trex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationBeTRexTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExaminationBeTRexTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trex/OnlineExaminationBeTRexTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExaminationBeTRexTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
