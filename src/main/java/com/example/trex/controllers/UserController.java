fa8e08662b7998207b9343a44048d60d36d47ec1
unknown <18130185@st.hcmuaf.edu.vn>
2022-04-21 22:52:54 +0700

    xu lý changePassword

--- src/main/java/com/example/trex/controllers/UserController.java
+++ src/main/java/com/example/trex/controllers/UserController.java
@@ -5,16 +5,14 @@ import com.example.trex.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RestController()
-@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class UserController {
-
-    //open setting handler
     @Autowired
     UserService userService;
 
 //    http://localhost:8080/change-pass/1
     @PutMapping("/change-pass/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
     public User changePass(@PathVariable(value = "id") Long id,@RequestBody String pass){
         User u = userService.getUserByID(id);
         if(u!=null){
