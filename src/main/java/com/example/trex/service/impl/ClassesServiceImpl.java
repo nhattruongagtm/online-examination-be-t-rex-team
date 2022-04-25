603ba3109815ce69f938033e0ef5db3c0bb37ebb
trung9266 <18130259@st.hcmuaf.edu.vn>
2022-04-25 19:43:24 +0700

    fix sun

--- src/main/java/com/example/trex/service/impl/ClassesServiceImpl.java
+++ src/main/java/com/example/trex/service/impl/ClassesServiceImpl.java
@@ -1,16 +1,16 @@
 package com.example.trex.service.impl;
 
 import com.example.trex.model.Classes;
+import com.example.trex.repository.ClassesRespository;
 import com.example.trex.service.ClassesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import sun.reflect.generics.repository.ClassRepository;
 
 import java.util.List;
 
 public class ClassesServiceImpl implements ClassesService {
 
     @Autowired
-    private ClassRepository classRepo;
+    private ClassesRespository classRepo;
 
     public List<Classes> getStudentId(Long studentId) {
         return null;
